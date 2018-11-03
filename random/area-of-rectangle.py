085650add967750405b577e1921651e1528f03fe
cristianbotezatu <44159579+cristianbotezatu@users.noreply.github.com>
2018-11-03 20:11:52 +0600

    Update area-of-rectangle.py
    semi-perimeter formula correction and used user input instead of predefined numbers

--- random/area-of-rectangle.py
+++ random/area-of-rectangle.py
@@ -1,8 +1,11 @@
-a = 5
-b = 6
-c = 7
+# inputs from the user
+a = float(input('Enter first side: '))
+b = float(input('Enter second side: '))
+c = float(input('Enter third side: '))
 
-sum = (a + b - c) / 2
+# the semi-perimeter
+sum = (a + b + c) / 2
 
+# the area
 area = (sum*(sum-a)*(sum-b)*(sum-c)) ** 0.5
 print('The area of the triangle is %0.2f' %area)
