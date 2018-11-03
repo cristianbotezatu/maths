f801989dd0a08af2851a629edceb405425ef0503
cristianbotezatu <44159579+cristianbotezatu@users.noreply.github.com>
2018-11-03 19:57:13 +0600

    Update Substraction.py
    corrected program replacing "+" with "-"

--- general-math/Substraction.py
+++ general-math/Substraction.py
@@ -1,6 +1,6 @@
 number1 = input('Enter 1st number: ')
 number2 = input('Enter 2nd number: ')
 
-substraction = float(number1) + float(number2)
+substraction = float(number1) - float(number2)
 
 print('The sum of {0} and {1} is {2}'.format(number1, number2, substraction))
